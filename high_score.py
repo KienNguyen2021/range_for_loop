c07ceb9df27a6da5756ad627c48410cc25759e49
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2021-04-25 12:21:40 -0600

    first commit

---
+++ high_score.py
@@ -0,0 +1,20 @@
+# student_score = input (" Enter a list of score : ").split()
+
+
+# # for n in range(0, len(sdudent_score)):
+# #     student_score[n] =  int(student_score[n])
+# print(student_score)    
+# student_score[] = int(student_score[])
+# print(max(student_score))
+
+# for x in range (2,10) :
+    
+#   print (x)  
+
+for x in range (1, 20, 2)  :
+    print (x)
+
+total = 0
+for x in range (200,300) :
+    total += x
+print (total)
